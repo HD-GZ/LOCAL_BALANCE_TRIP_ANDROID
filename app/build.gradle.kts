c0ff57cd45cb4e9dd0924cbcf62bb980bbec4ca1
WooJin Kong <kongjak@kongjak.dev>
2026-08-12 14:38:41 +0900

    chore: Set version to 0.0.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,7 +25,7 @@ android {
     defaultConfig {
         applicationId = "live.lb_trip.localbalancetrip"
         versionCode = 1
-        versionName = "1.0"
+        versionName = "0.0.1"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
